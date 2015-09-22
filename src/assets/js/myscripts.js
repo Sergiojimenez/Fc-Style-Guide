4cefa9d2a84ad2300f9a87b1a35298d7a0ac044d
Joan Lafulla <joan.lafulla@scmspain.com>
2015-09-22 13:15:37 +0200

    add octopus

---
+++ src/assets/js/myscripts.js
@@ -0,0 +1,16 @@
+//Load content
+// $(function(){
+// 	var loadContent = function(wrapperLoad, url, target) {
+// 		wrapperLoad = $(wrapperLoad);
+// 	 	wrapperLoad.html("<div class='loader' id='spinner'>Loading...</div>");
+// 	 	wrapperLoad.load(url);	
+// 	}
+
+// 	$("#mainNav").on("click", "a", function(e){
+// 		e.preventDefault();
+// 		console.log("ahi va");
+// 		var wrapperLoad = $("#main");
+// 		var url = "base.html";
+// 			loadContent(wrapperLoad, url);
+// 	});
+// });
